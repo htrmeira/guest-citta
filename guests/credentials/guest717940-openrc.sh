e03764932b1bfe71aaa21c7597d0d0a5922e4afd
Heitor Meira <heitor@silibrina.com>
2014-09-15 18:12:30 -0300

    changing the way the deletion occurs, to execute without loading admin credentials

--- guests/credentials/guest717940-openrc.sh
+++
@@ -1,5 +0,0 @@
-export OS_USERNAME=guest717940
-export OS_PASSWORD=TciKvBlTV7w3V
-export OS_EMAIL=htrmeira@gmail.com
-export OS_TENANT_NAME=guest717940
-export OS_AUTH_URL=http://10.0.0.14:35357/v2.0
--- guests/credentials/guest795095-openrc.sh
+++
@@ -1,5 +0,0 @@
-export OS_USERNAME=guest795095
-export OS_PASSWORD=onABYC_bvo4xt
-export OS_EMAIL=htrmeira@gmail.com
-export OS_TENANT_NAME=guest795095
-export OS_AUTH_URL=http://10.0.0.14:35357/v2.0
